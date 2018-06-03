6fde7673122887e90638e8493907d0946f6741de
Juri <juriahn817@gmail.com>
2018-06-03 12:59:17 -0500

    /home: Auto stash before merge of "features/home" and "develop"

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import Header from '../Header';
 import styled from 'styled-components';
+import Avatar from './AvatarIcon';
 
 const Wrapper = styled.div`
   margin: 0px;
   padding: 0px;
   width: 100%;
+  text-align: center;
 `;
+
+const Title = styled.h1`
+  color: red;
+`;
+
+
 
 const HomePage = () => (
   <Wrapper>
     <Header/>
-    <h1>TEST</h1>
+    <Title>DWELLO</Title>
+    <p>Welcome to Dwello! Make a custom list for your household.</p>
+    <Avatar letter="D"/>
   </Wrapper>
 );
 
